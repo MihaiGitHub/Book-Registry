5f3c712b3b41555375bbf012524cc1945cb8c23a
MihaiSmarandache <mihai.sanfran@gmail.com>
2018-08-20 20:56:32 -0700

    Create BooksReducer

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function(){
+    return [
+        { title: 'Learn Javascript' },
+        { title: 'Harry Potter' },
+        { title: 'The Dark Tower' },
+        { title: 'Eloquent Ruby' }
+    ]
+}
