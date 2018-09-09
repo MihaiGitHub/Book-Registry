e4507ed893b18c984b63496681d2c67bb1129b04
MihaiSmarandache <mihai.sanfran@gmail.com>
2018-09-09 09:43:30 -0700

    Implement container class

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class BookList extends Component {
+class BookList extends Component {
     renderList(){
         return this.props.books.map((book) => {
             return (
@@ -17,3 +18,16 @@ export default class BookList extends Component {
         )
     }
 }
+
+// It takes in the entire application state; If state changes then entire component will rerender
+function mapStateToProps(state) {
+    // Whatever is returned will show up as props
+    // Inside of BookList
+    return {
+        // Everytime app state changes the object in state function will be assigned as props in component
+        books: state.books
+    };
+}
+
+// Export 2 params
+export default connect(mapStateToProps)(BookList);
