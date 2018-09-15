7415e0b27bd9798aabc730be2af2ffc33998b3ac
MihaiSmarandache <mihai.sanfran@gmail.com>
2018-09-15 14:32:49 -0700

    Update action to return type and payload of book

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,8 @@
 export function selectBook(book){
-    console.log(book.title);
+    // selectBook is an ActionCreator, it needs to return an action
+    // an object with a type property
+    return { // An action always contains a type and sometimes a payload
+        type: 'BOOK_SELECTED',
+        payload: book
+    };
 }
