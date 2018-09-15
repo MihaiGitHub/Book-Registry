a98c346c8028a94f0e080c0ac83ba9530f51f17c
MihaiSmarandache <mihai.sanfran@gmail.com>
2018-09-15 15:10:29 -0700

    Consume action in active book reducer

---
+++ src/reducers/reducer_active_book.js
@@ -0,0 +1,12 @@
+// All reducers get two arguments; hte current state and action
+// Reducers are only called when an action occurs
+// State argument is not application state, on the state this reducer
+// is responsible for
+export default function(state = null, action){ // When argument is undefined set to null
+    switch (action.type){
+        case 'BOOK_SELECTED':
+            return action.payload;
+    }
+
+    return state;
+}
